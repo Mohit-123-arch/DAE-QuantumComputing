592a371abeaf888f62c9c3e521d16c62053432f5
HS03 <hs03@Skills-Academy-53.local>
2026-07-15 10:56:16 -0700

    first commit

---
+++ python_2/python.py
@@ -0,0 +1,20 @@
+Integer = 2
+String = "Hello"
+Boolean = True
+
+if Integer > 3:
+    print("Integer is greater than 3")
+else:   
+    print("Integer is not greater than 3")
+
+for Room in range(1, 11):
+    print(f"Room {Room} checked")
+
+# Add number from 0 to 500 using for loops and print sum
+
+sum = 0
+for number in range(1238098):
+    sum = sum + number
+print(sum)
+
+range(10-1)
